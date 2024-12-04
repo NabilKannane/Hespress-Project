388c1122d6d9c76af00fb8f9bedff09011b692b8
NabilKannane <nabilkannane1@gmail.com>
2024-12-04 18:53:34 +0100

    styling simplinechart

--- app/components/plots/SimplineChart.js
+++ app/components/plots/SimplineChart.js
@@ -8,6 +8,25 @@ const SimplineChart = ({ data }) => {
 // console.log(data)
   }, [])
   
+// Fonction personnalisée pour le tooltip
+const CustomTooltip = ({ active, payload, label }) => {
+  if (active && payload && payload.length) {
+    return (
+      <div className="bg-slate-700/70 p-4 rounded-xl shadow-xl">
+        <p className="text-white text-sm">{`Time: ${label}`}</p>
+        {payload.map((entry, index) => (
+          <p key={index} className="text-sm">
+            <span className="font-semibold" style={{ color: entry.stroke }}>{`${entry.name} : `}</span>
+            <span style={{ color: entry.stroke }}>{entry.value}</span>
+          </p>
+        ))}
+      </div>
+    );
+  }
+
+  return null;
+};
+
 
   return (
     <ResponsiveContainer width="100%" height="100%">
@@ -22,15 +41,15 @@ const SimplineChart = ({ data }) => {
           bottom: 5,
         }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="3 3" vertical={true} horizontal={true} stroke='rgb(148 163 184 / 0.2)'/>
         <XAxis dataKey="time" />
         <YAxis />
-        <Tooltip />
+        <Tooltip content={<CustomTooltip />} />
         <Legend />
-        <Line type="monotone" dataKey="Negative" stroke=" rgb(239 68 68)" />
-        <Line type="monotone" dataKey="Neutral" stroke="rgb(120 113 108)" />
-        <Line type="monotone" dataKey="Positive" stroke="rgb(34 197 94)" />
-      </LineChart>
+        <Line type="monotone" dataKey="Negative" stroke=" rgb(239 68 68)" dot={false} />
+        <Line type="monotone" dataKey="Neutral" stroke="rgb(120 113 108)" dot={false}/>
+        <Line type="monotone" dataKey="Positive" stroke="rgb(34 197 94)" dot={false}/>
+      </LineChart >
     </ResponsiveContainer>
   );
 };
